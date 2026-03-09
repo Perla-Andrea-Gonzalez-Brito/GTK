359346ff51851f86740a456d83ef60e1ac8ef212
Perla-Andrea-Gonzalez-Brito <al2163074559@azc.uam.mx>
2026-03-09 05:43:13 -0600

    main.c

--- main.c
+++ main.c
@@ -1,39 +1,3 @@
-#include <gtk/gtk.h>
-
-/* funcion que se ejecuta cuando se presiona el boton */
-static void boton_presionado(GtkWidget *widget, gpointer data)
-{
-    GtkWidget *dialog;
-
-    dialog = gtk_message_dialog_new(NULL,
-                                    GTK_DIALOG_MODAL,
-                                    GTK_MESSAGE_INFO,
-                                    GTK_BUTTONS_OK,
-                                    "Presionaste el boton");
-
-    gtk_dialog_run(GTK_DIALOG(dialog));
-    gtk_widget_destroy(dialog);
-}
-
-/* funcion principal que crea la ventana */
-static void activate(GtkApplication *app, gpointer user_data)
-{
-    GtkWidget *window;
-    GtkWidget *button;
-
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "GTK SO");
-    gtk_window_set_default_size(GTK_WINDOW(window), 350, 350);
-
-    button = gtk_button_new_with_label("Presiona este boton");
-
-    g_signal_connect(button, "clicked", G_CALLBACK(boton_presionado), NULL);
-
-    gtk_container_add(GTK_CONTAINER(window), button);
-
-    gtk_widget_show_all(window);
-}
-
 int main(int argc, char **argv)
 {
     GtkApplication *app;
@@ -49,8 +13,4 @@ int main(int argc, char **argv)
 
     return status;
 }
-    status = g_application_run(G_APPLICATION(app), argc, argv);
-
-    g_object_unref(app);
-    return status;
-}
+ 
